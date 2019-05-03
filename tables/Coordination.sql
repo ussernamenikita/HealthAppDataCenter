6f538304cb23074acf57b2f73f0a651f5afa5f5a
Nikita Bulygin <nikitaatk@gmail.com>
2019-05-04 01:57:59 +0300

    Set coordination fiels as optional.Switch symbol type to VARCHAR in TextWatcher

--- tables/Coordination.sql
+++ tables/Coordination.sql
@@ -2,13 +2,13 @@ CREATE TABLE Coordination
 (
    user_id	VARCHAR   NOT NULL,
    time         TIMESTAMP NOT NULL,
-   pitch DOUBLE NOT NULL,
-   azimut DOUBLE NOT NULL,
-   roll DOUBLE NOT NULL,
-   latitude DOUBLE NOT NULL,
-   longitude DOUBLE NOT NULL,
-   altiture DOUBLE NOT NULL,
+   pitch DOUBLE ,
+   azimut DOUBLE,
+   roll DOUBLE ,
+   latitude DOUBLE ,
+   longitude DOUBLE ,
+   altiture DOUBLE ,
    PRIMARY KEY (
      (user_id, QUANTUM(time, 15, 'm')),
-      user_id, time,symbol)
+      user_id, time)
 )
--- tables/TextWatcher.sql
+++ tables/TextWatcher.sql
@@ -2,7 +2,7 @@ CREATE TABLE TextWatcher
 (
    user_id	VARCHAR   NOT NULL,
    time         TIMESTAMP NOT NULL,
-   symbol     DOUBLE   NOT NULL,
+   symbol     VARCHAR   NOT NULL,
    timeDelta  SINT64 NOT NULL,
    PRIMARY KEY (
      (user_id, QUANTUM(time, 15, 'm')),
